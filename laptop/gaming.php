fbfe9c2556559e9dbb8c281162324991c0447f91
ThoNoPro21 <109445487+ThoNoPro21@users.noreply.github.com>
2022-09-12 16:56:38 +0700

    Add files via upload

---
+++ laptop/gaming.php
@@ -0,0 +1,49 @@
+<?php ?>
+<div class="content">
+              <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+                <div class="col">
+                  <div class="card h-100 shadow-sm"> <img src="../laptop/gaming/img-gaming/1.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-success">15.000.000 VND</span>  </div>
+                      <h5 class="card-title">máy 1</h5>
+                      <div class="text-center my-4 force-to-bottom "> <a href="#" class="btn btn-warning">Check offer</a> </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="card h-100 shadow-sm"> <img src="../laptop/gaming/img-gaming/2.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-success">10.999.000 VND</span>  </div>
+                      <h5 class="card-title">máy 2</h5>
+                      <div class="text-center my-4" > <a href="#" class="btn btn-warning">Check offer</a> </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="card h-100 shadow-sm"> <img src="../laptop/gaming/img-gaming/3.webp" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+                      <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-success">11.500.999 VND</span>  </div>
+                      <h5 class="card-title">máy 3</h5>
+                      <div class="text-center my-4"> <a href="#" class="btn btn-warning">Check offer</a> </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="card h-100 shadow-sm"> <img src="../laptop/gaming/img-gaming/4.webp" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+                      <div class="clearfix mb-3"> <span class="float-start badge rounded-pill bg-success">12.000.000 VND</span>  </div>
+                      <h5 class="card-title">máy 4</h5>
+                      <div class="text-center my-4"> <a href="#" class="btn btn-warning">Check offer</a> </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+</body>
+</html>
